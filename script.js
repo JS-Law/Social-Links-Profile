6ef90f5e721938d330e841cc66439abde68a11b3
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-02-19 06:49:04 -0500

    Fix footer, add remaining eventlisteners add hover state, add desktop queries, add headshot

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let btnThree = document.querySelector('.mediaCardThree');
 let btnFour = document.querySelector('.mediaCardFour');
 let btnFive = document.querySelector('.mediaCardFive');
 
+
+// GitHub
 btnOne.addEventListener('click', () => {
     window.location.href = 'https://github.com/JS-Law';
 });
@@ -12,6 +14,7 @@ btnOne.addEventListener('ontouch', () => {
 });
 
 
+// FrontendMentor
 btnTwo.addEventListener('click', () => {
     window.location.href = 'https://www.frontendmentor.io/profile/JS-Law';
 });
@@ -22,29 +25,29 @@ btnTwo.addEventListener('ontouch', () => {
 
 // LinkedIn
 btnThree.addEventListener('click', () => {
-    window.location.href = '';
+    window.location.href = 'https://www.linkedin.com/in/jeff-stanley-25153a217/';
 });
 
 btnThree.addEventListener('ontouch', () => {
-    window.location.href = '';
+    window.location.href = 'https://www.linkedin.com/in/jeff-stanley-25153a217/ ';
 });
 
 
 // Twitter
 btnThree.addEventListener('click', () => {
-    window.location.href = '';
+    window.location.href = 'https://twitter.com/justdevposts';
 });
 
 btnThree.addEventListener('ontouch', () => {
-    window.location.href = '';
+    window.location.href = 'https://twitter.com/justdevposts';
 });
 
 
 // Instagram
 btnThree.addEventListener('click', () => {
-    window.location.href = '';
+    window.location.href = 'https://www.instagram.com/bluecollarjeff_/?igsh=b3gwMXJyMGI4NjEw&utm_source=qr';
 });
 
 btnThree.addEventListener('ontouch', () => {
-    window.location.href = '';
+    window.location.href = 'https://www.instagram.com/bluecollarjeff_/?igsh=b3gwMXJyMGI4NjEw&utm_source=qr';
 });
