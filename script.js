76ed32d0f78b98d15078a4676fe9638418ee9a21
Jeff Stanley <10554861+JS-Law@users.noreply.github.com>
2024-02-18 21:23:43 -0500

    Add eventlisteners to btnOne btnTwo

---
+++ script.js
@@ -0,0 +1,50 @@
+let btnOne = document.querySelector('.mediaCardOne');
+let btnTwo = document.querySelector('.mediaCardTwo');
+let btnThree = document.querySelector('.mediaCardThree');
+let btnFour = document.querySelector('.mediaCardFour');
+let btnFive = document.querySelector('.mediaCardFive');
+
+btnOne.addEventListener('click', () => {
+    window.location.href = 'https://github.com/JS-Law';
+});
+btnOne.addEventListener('ontouch', () => {
+    window.location.href = 'https://github.com/JS-Law';
+});
+
+
+btnTwo.addEventListener('click', () => {
+    window.location.href = 'https://www.frontendmentor.io/profile/JS-Law';
+});
+btnTwo.addEventListener('ontouch', () => {
+    window.location.href = 'https://www.frontendmentor.io/profile/JS-Law';
+});
+
+
+// LinkedIn
+btnThree.addEventListener('click', () => {
+    window.location.href = '';
+});
+
+btnThree.addEventListener('ontouch', () => {
+    window.location.href = '';
+});
+
+
+// Twitter
+btnThree.addEventListener('click', () => {
+    window.location.href = '';
+});
+
+btnThree.addEventListener('ontouch', () => {
+    window.location.href = '';
+});
+
+
+// Instagram
+btnThree.addEventListener('click', () => {
+    window.location.href = '';
+});
+
+btnThree.addEventListener('ontouch', () => {
+    window.location.href = '';
+});
